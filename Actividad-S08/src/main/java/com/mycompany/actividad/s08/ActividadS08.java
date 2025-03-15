abcbbf4fa8750cb301130caf7e04a3c7e0b1677a
LeonidasVS <villafrancosibrian@gmail.com>
2025-03-15 11:57:32 -0600

    Documentacion

--- Actividad-S08/src/main/java/com/mycompany/actividad/s08/ActividadS08.java
+++ Actividad-S08/src/main/java/com/mycompany/actividad/s08/ActividadS08.java
@@ -33,16 +33,28 @@ import java.util.List;
 public class ActividadS08 {
 
     public static void main(String[] args) {
+        
+        //Intefaz Animales
         InterfaceGatuna gato= new InterfaceGatuna();
         InterfacePerruna perro=new InterfacePerruna();
+        
+        //Interfaz Vehiculo
         InterfaceBicicleta b= new InterfaceBicicleta();
         InterfaceCoche c=new InterfaceCoche();
+        
+        //Interfaz de pago
         PagoConTarjeta pct=new PagoConTarjeta();
         PagoEfectivo pe=new PagoEfectivo();
+        
+        //Interfaz de FiguraGeometrica
         Circulo circulo=new Circulo(3);
         Rectangulo rec=new Rectangulo(2,2);
+        
+        //Interfaz de trabajador
         Desarrollador desa=new Desarrollador();
         Diseñador diseño=new Diseñador();
+        
+        //Interfaz de ordenable
         List<Integer> numeros=new ArrayList();
         numeros.add(100);
         numeros.add(50);
@@ -50,31 +62,58 @@ public class ActividadS08 {
         numeros.add(12);
         numeros.add(11);
         ListarNumeros lista= new ListarNumeros(numeros);
+        
+        //Interfaz Alimentacion
         Animal ani=new Animal();
         Persona persona=new Persona();
+        
+        //Interfaz Notidicacion
         SMS sms =new SMS();
         CorreoElectronico correo=new CorreoElectronico();
+        
+        //Interfaz Comparacion de objetos
         Producto pro1=new Producto(19);
         Producto pro2=new Producto(19);
+        
+        //Interfaz de Calcular Descuento
         DescuentoFijo desf=new DescuentoFijo(5);
         DescuentoPorcentaje desp=new DescuentoPorcentaje(12);
         
+        
+        
+        
+        //Imprimir Interfaz Animales
         System.out.println(perro.hacerSonido(12));
         System.out.println(gato.mover(10));
+        
+        //Imprimir Interfaz Vehiculo
         System.out.println(b.detener("el parque"));
         System.out.println(c.arrancar(50));
+        
+        //Imprimit Interfaz Pago
         System.out.println(pct.procesarPago(5.78));
         System.out.println(pe.procesarPago(1.25));
+        
+        //Imprimir Interfaz de FiguraGeometrica
         System.out.println(circulo.perimetro());
         System.out.println(rec.area());
+        
+        //Imprimir Interfaz de trabajador
         System.out.println(desa.trabajar("Leonidas"));
         System.out.println(diseño.trabajar("Juvini"));
+        
+        //Imprimir Interfaz de ordenable
         System.out.println(lista.Ordenable());
+        
+        //Imprimir Interfaz Alimentacion
         System.out.println(ani.comer(2));
         System.out.println(persona.comer(6));
+        
+        //Imprimir Interfaz Notidicacion
         System.out.println(sms.enviarNotificacion());
         System.out.println(correo.enviarNotificacion());
         
+        //Imprimir Interfaz Comparacion de objetos
         int resultado=pro1.comparacion(pro2);
         if (resultado < 0) {
             System.out.println("Producto1 es más barato que Producto2");
@@ -84,6 +123,7 @@ public class ActividadS08 {
             System.out.println("Ambos productos tienen el mismo precio");
         }
         
+        //Imprimir Interfaz de Calcular Descuento
         double precionormal=69;
         
         System.out.println("Precio original: "+precionormal);
